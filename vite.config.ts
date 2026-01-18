0cf296994a59cabd623028f51c55ae9a569142f9
task58 <taskr3g@gmail.com>
2026-01-19 08:10:32 +0900

    inital deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react({
       babel: {
